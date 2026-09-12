617a1774c44e9381aedf3156ee7a45b9f3bc5bb6
varsha <kanakdandevarsha@gmail.com>
2026-09-12 16:03:12 +0530

    Fix service route middleware filename case

--- server/routes/ServiceRoutes.js
+++ server/routes/ServiceRoutes.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const{createService,getAllServices,getservicebyId}=require("../controllers/servicecontroller.js");
 const protect=require("../middleware/authMiddleware.js");
 //const authorizeroles=require("../middleware/rolemiddleware");
-const authorizeRoles = require("../middleware/rolemiddleware");
+const authorizeRoles = require("../middleware/roleMiddleware");
 router.post(
     "/",
     protect,
